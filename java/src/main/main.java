ea704260151fc53f5303f7f996f1423c488b990b
ppanero <ppanero27@gmail.com>
2014-07-16 15:19:45 +0200

    Check input

--- java/src/main/main.java
+++ java/src/main/main.java
@@ -12,9 +12,24 @@ public class main {
     public static void main(String [] args)
     {
         //ask for a number to factorize
-        Scanner input = new Scanner(System.in);
-        BigInteger nToFactorize = input.nextBigInteger();
-        System.out.println("One posible factor is: " + rhoalgorithm.factorize(nToFactorize, 400000).toString());
+        try {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Type the number to factorize: ");
+            BigInteger nToFactorize = input.nextBigInteger();
+            System.out.println("One posible factor is: " + rhoalgorithm.factorize(nToFactorize, 100000000).toString());
+        }
+        catch(java.util.InputMismatchException e){
+            System.out.println("Input mismatch exception. Input must be a positive integer");
+        }
+        catch(java.util.NoSuchElementException e){
+            System.out.println("No such element exception");
+        }
+        catch(IllegalStateException e){
+            System.out.println("Illegal state exception occured");
+        }
+        catch(Exception e){
+            System.out.println("An unhandled exception occured...Sorry :(");
+        }
 
     }
 }
